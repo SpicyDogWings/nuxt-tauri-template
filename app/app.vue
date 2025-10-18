4330738d6493dc0eb5d4eb57d56f1c6588cd6d81
SpicyDogWings <168324845+SpicyDogWings@users.noreply.github.com>
2025-10-18 11:09:51 -0600

    fix(deps): antes de tailwind

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="py-y bg-blue-500">
+    <h1 class="text-blue-500">Hola</h1>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
+  future: {
+    compatibilityVersion: 4
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  ssr: false
+  ssr: false,
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+    ]
+  },
 })
