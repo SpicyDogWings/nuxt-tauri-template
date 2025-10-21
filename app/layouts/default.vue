49cea71c365304b2051f2ef14cbb4ee802f028b5
SpicyDogWings <168324845+SpicyDogWings@users.noreply.github.com>
2025-10-20 19:02:39 -0600

    fix(vue): jeje se me olvidó

---
+++ app/layouts/default.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui'
+const items = ref<NavigationMenuItem[][]>([
+  [
+    {
+      label: 'Home',
+      icon: 'lucide:house',
+      to: "/"
+    }
+  ],
+])
+</script>
+
+<template>
+    <div class="flex justify-start items-start min-h-screen">
+        <UNavigationMenu collapsed orientation="vertical" :items="items" class="data-[orientation=vertical]:w-fit" />
+        <div class="flex flex-col justify-start items-start">
+            <slot />
+        </div>
+    </div>
+</template>
