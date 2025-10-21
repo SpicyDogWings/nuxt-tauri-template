95bac6393453d6b17dd26a82863659850c2064a9
SpicyDogWings <168324845+SpicyDogWings@users.noreply.github.com>
2025-10-20 19:02:08 -0600

    faet(dep): pack de inicio - tailwind - tauri

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="py-y bg-surface-900">
-    <h1 class="text-surface-300">Hola</h1>
-    <Icon class="text-surface-300" name="lucide:a-arrow-down"></Icon>
-  </div>
+    <main class="bg-surface-950">
+        <NuxtLayout>
+            <NuxtPage />
+        </NuxtLayout>
+    </main>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,20 +5,25 @@ export default defineNuxtConfig({
   future: {
     compatibilityVersion: 4
   },
-
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   ssr: false,
-
   css: [
-    "~~/assets/css/main.css"
+    "~/assets/css/main.css"
   ],
-
   vite: {
+    clearScreen: false,
+    envPrefix: ['VITE_', 'TAURI_'],
+    server: {
+      strictPort: true,
+    },
     plugins: [
       tailwindcss(),
     ]
   },
-
+  devServer: {
+    host: '0',
+  },
   modules: ["@nuxt/icon", "@nuxt/fonts", "@nuxt/ui"],
+  ignore: ['**/src-tauri/**'],
 })
